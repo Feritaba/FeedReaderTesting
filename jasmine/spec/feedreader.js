b3516a351196baa3d0c92ac5e021cdcd57d815fb
foroozan akhavan <foroozan.akhavan@gmail.com>
2018-08-07 12:16:24 -0500

    6th test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -62,6 +62,7 @@ $(function() {
 			const body = document.querySelector('body');
 			expect(body.classList.contains('menu-hidden')).toBe(true);
 		});
+
 		/* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
@@ -78,9 +79,9 @@ $(function() {
 			expect(body.classList.contains('menu-hidden')).toBe(true);
 		});
 	});
+
 	/* TODO: Write a new test suite named "Initial Entries" */
 	describe('Initial Entries', function(){
-
 
 		/* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -88,7 +89,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+		beforeEach(function(done){
+			loadFeed(0, done);
+		});
+		it('completes work', function(){
+			const feed = document.querySelector('.feed');
+			expect(feed.children.length > 0).toBe(true);
+		});
 	});
+
 	/* TODO: Write a new test suite named "New Feed Selection" */
 	describe('New Feed Selection', function(){
 		/* TODO: Write a test that ensures when a new feed is loaded
